b493b1a61046462027a415b9a9a10f96e1872d54
hamzausmani302 <hamzausmani021@gmail.com>
2022-08-14 19:11:26 +0500

    added mailng sevice and video chat capabilities

---
+++ public/js/ejs.js
@@ -0,0 +1,63 @@
+
+
+
+$(document).ready(()=>{
+    document.querySelector("#checkbox").addEventListener("change" , ()=>{
+        if(document.getElementById("checkbox").checked === true){
+            $("#participants").prop('disabled' ,false);
+        }else{
+            $("#participants").prop('disabled' ,true);
+            
+        }
+    })
+    
+    
+})
+
+
+function startMeeting(){
+    console.log("dawd");
+    let roomId = document.querySelector("#roomid").value;
+    if(roomId == ""){
+        console.log("roomID should not be empty");
+    }
+
+    
+
+
+    window.location.href = `/${roomId}`;
+    
+}
+
+function startANewMeeting(){
+    // window.location.href = "/room/create";
+    if(document.getElementById("checkbox").checked === true){
+        let inputData = document.querySelector("#participants").value;
+        const emails = inputData.split("\n");
+        const URL = "/invite";
+        const method = "POST"
+        const requestBody = {
+            "invites": emails
+        }
+        //check email validitiy
+        //send post request to send emails to the followin emails
+        fetch(URL , {
+            method : method,
+            headers:{
+                "content-type":"application/json"
+            },
+            body : JSON.stringify(requestBody)
+        }).then(data=>{
+
+        }).catch(err=>{
+
+        })
+
+        
+
+    }
+
+    // window.location.href= "/room/create";
+
+}
+
